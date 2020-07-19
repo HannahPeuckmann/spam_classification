ea17f5b306699daa6e159d23960d5bc6a0493d63
HannahPeuckmann <66631993+HannahPeuckmann@users.noreply.github.com>
2020-07-19 12:29:40 +0200

    added feature extraction
    Restructured the code, renamed class to BasicNlp. Feature extraction: whitespace, uppercase, special characters, hyphens. Still on single file level.

--- TxtProcess_class.py
+++ TxtProcess_class.py
@@ -9,10 +9,12 @@ import nltk
 
 import logging
 
+
+
 # multiprocessing!
 
 
-class TxtNLP:
+class BasicNLP:
     def __init__(self,filename):
         self.model = spacy.load('en_core_web_sm')
         self.sentences = None
@@ -20,37 +22,61 @@ class TxtNLP:
         self.tags = []
         self.lemma = []
         f = open(filename, mode='r')
-        full_text = ''
+        self.full_text = ''
         for line in f:
             line = line.strip()
-            full_text = full_text + ' ' + line
+            self.full_text = self.full_text + ' ' + line
         f.close()
-        self.sentences = [sentence for sentence in nltk.sent_tokenize(full_text)]
-        for sentence in self.sentences:
-            self.process_sentence(sentence)
 
-    def process_sentence(self, sentence):
-        process_sentences = self.model(sentence)
-        # tokenize sentences
-        self.tokens.append([token.text for token in process_sentences])
+    def basic_process_file(self):
+        self.sentences = [sentence for sentence in nltk.sent_tokenize(self.full_text)]
+        for sentence in self.sentences:
+            spacy_sentence = self.model(sentence)
+            self._process_sentence(spacy_sentence)
+
+    def _process_sentence(self, sentence):
+        # tokens
+        self.tokens.append([token.text for token in sentence])
         logging.debug(self.tokens)
-        self.tags.append([token.tag_ for token in process_sentences])
+        # tags
+        self.tags.append([token.tag_ for token in sentence])
         logging.debug(self.tags)
-        self.lemma.append([token.lemma_ for token in process_sentences])
+        # lemmas
+        self.lemma.append([token.lemma_ for token in sentence])
         logging.debug(self.lemma)
 
-    def write(self, filename):
+    def write_to_file(self, filename):
         with open(filename, mode='w') as file:
             for j in range(len(self.tokens)):
                 for i in range(len(self.tokens[j])):
                     file.write(self.tokens[j][i] + '\t' + self.tags[j][i] + '\t' + self.lemma[j][i] + '\n')
                 file.write('\n')
 
+def extract_features(text):
+    count_up = 0
+    count_special = 0
+    count_white = 0
+    count_hyphens = 0
+    for char in text:
+        if char.isupper():
+            count_up += 1
+        if not char.isalpha():
+            if char.isspace():
+                count_white += 1
+            else:
+                count_special += 1
+                if char == '-':
+                    count_hyphens += 1
+    return count_up, count_special, count_hyphens, count_white
+
+
+
+def write_features(self, filename):
+    pass
 
 if __name__ == "__main__":
     logging.basicConfig(filename='enron_log.log',level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
-    test = TxtNLP('0006.2003-12-18.GP.spam.txt')
-    #test.process()
-    #test.tokenize()
-    #test.tagging()
-    test.write('nlp_processed.txt')
+    test = BasicNLP('0006.2003-12-18.GP.spam.txt')
+    test.basic_process_file()
+    test.write_to_file('Data/enron_ttl_testdata/ttl_spam_test/0006.2003-12-18.GP.spam.txt')
+    logging.debug(extract_features(test.full_text))
